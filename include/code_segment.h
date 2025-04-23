aac464487092612e602eb30d84b9d23f332573b3
Hassan EL KHARROUBI <elkharroubihassan@outlook.com>
2025-04-23 19:18:23 +0100

    add signature to .h

--- include/code_segment.h
+++ include/code_segment.h
@@ -3,3 +3,9 @@
 
 int search_and_replace(char **str, HashMap *values);
 char *trim(char *str) ;
+int resolve_constants(ParserResult *result) ;
+void allocate_code_segment(CPU *cpu, Instruction **code_instructions, int code_count) ;
+int handle_instruction(CPU *cpu, Instruction *instr, void *src, void *dest) ;
+int execute_instruction(CPU *cpu, Instruction *instr) ;
+Instruction *fetch_next_instruction(CPU *cpu);
+int run_program(CPU *cpu) ;
